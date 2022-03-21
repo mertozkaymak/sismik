9a8ab30578396f9f4e347e2a33eee8a0858d30be
Mert Özkaymak <101987240+mertozkaymak@users.noreply.github.com>
2022-03-21 03:04:15 +0300

    Update api.config.php

--- ideasoft/api.config.php
+++ ideasoft/api.config.php
@@ -2,15 +2,15 @@
 
     //Database Settings
     define("HOST", "localhost");
-    define("DBNAME", "sismik-excel");
-    define("USER", "sismik-excel");
-    define("PASS", "Digital1357*c");
+    define("DBNAME", "***");
+    define("USER", "***");
+    define("PASS", "***");
 
     //Shop Settings
-    define("URL", "https://sismikmarket.myideasoft.com");
-    define("CLIENTID", "18_53is0wh89twk8sswc80occk8os8ggws4cw4sksscggckwg000s");
-    define("SECRETID", "61k2izze5zswow4k04skgwocs8o8os4s8ko4wok4coo0k48ocw");
-    define("REDIRECT", "https://dev.digitalfikirler.com/sismik-excel/controller.php");
-    define("ACCESS", URL . "/admin/user/auth?client_id=" . CLIENTID . "&response_type=code&state=d12b2a61b81a52be4a4cac7e610f62f8&redirect_uri=" . REDIRECT);
+    define("URL", "***");
+    define("CLIENTID", "***");
+    define("SECRETID", "***");
+    define("REDIRECT", "***");
+    define("ACCESS", URL . "/admin/user/auth?client_id=" . CLIENTID . "&response_type=code&state=***&redirect_uri=" . REDIRECT);
 
 ?>
